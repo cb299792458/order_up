2d2e1d9e3695517eeead5ce2df00484d66f8dfd5
brian <brianrlam@gmail.com>
2023-07-17 15:46:35 -0400

    finish login and logout

---
+++ app/config.py
@@ -0,0 +1,8 @@
+import os
+from os import environ
+
+
+class Configuration:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI=environ.get("DATABASE_URL") or "postgresql://order_up:9uCxydbt@localhost/order_up_dev"
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
