ab80ee5dcfc76b065d7d923967947c7dc5ac1b14
brian <brianrlam@gmail.com>
2023-07-19 15:41:44 -0400

    make assign table form (doesn't work)

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, SelectField
 from wtforms.validators import DataRequired
+# from .models import db, Employee, Table
 
 class LoginForm(FlaskForm):
     employee_number = StringField('Employee Number', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Save')
+    submit = SubmitField('Log In')
+
+class TableAssignmentForm(FlaskForm):
+    tables = SelectField("Tables", coerce=int)
+    servers = SelectField("Servers", coerce=int)
+    assign = SubmitField("Assign")
--- app/routes/orders.py
+++ app/routes/orders.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
-from app.models import Table
+from app.models import Table, Employee
+from app.forms import TableAssignmentForm
 from app import db
 
 bp = Blueprint("orders", __name__, url_prefix="")
@@ -10,4 +11,8 @@ bp = Blueprint("orders", __name__, url_prefix="")
 @login_required
 def index():
     tables = db.session.query(Table).all()
-    return render_template('orders.html', tables=tables)
+    employees = db.session.query(Employee).all()
+    form = TableAssignmentForm()
+    form.tables.choices=[(t.id,t.id) for t in tables]
+    form.servers.choices=[(e.id,e.name) for e in employees]
+    return render_template('orders.html', tables=tables, form=form)
