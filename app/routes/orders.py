715217e8b595e669ab62e8a59a2cdb323b74ac10
brian <brianrlam@gmail.com>
2023-07-19 19:55:29 -0400

    make close table button

--- app/routes/orders.py
+++ app/routes/orders.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for
-from flask_login import login_required
+from flask_login import login_required, current_user
 from app.models import Table, Employee, Order
 from app.forms import TableAssignmentForm
 from app import db
@@ -22,6 +22,9 @@ def index():
     form.table.choices=[(t.id,t.id) for t in open_tables]
     form.employee.choices=[(e.id,e.name) for e in employees]
 
+    your_id = int(current_user.get_id())
+    your_tables = [table for table in tables if table.orders and table.orders[0].employee.id==your_id]
+
     if form.validate_on_submit():
         table=form.table.data
         employee=form.employee.data
@@ -30,4 +33,16 @@ def index():
         db.session.commit()
         return redirect(url_for('.index'))
 
-    return render_template('orders.html', tables=tables, form=form)
+    return render_template('orders.html', tables=tables, form=form, your_tables=your_tables)
+
+@bp.route("/close_table/<int:id>",methods=['POST'])
+@login_required
+def close(id):
+    order = Table.query.get(id).orders[0]
+    for detail in order.details:
+        db.session.delete(detail)
+
+    db.session.delete(order)
+    db.session.commit()
+
+    return redirect(url_for('.index'))
