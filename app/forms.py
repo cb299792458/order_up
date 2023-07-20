acbd7b7cc5ae230a2de074e78e5eaf1dc33a6759
brian <brianrlam@gmail.com>
2023-07-20 15:19:30 -0400

    finish order form

--- app/forms.py
+++ app/forms.py
@@ -1,14 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, SelectField
 from wtforms.validators import DataRequired
-# from .models import db, Employee, Table
 
 class LoginForm(FlaskForm):
     employee_number = StringField('Employee Number', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Log In')
 
+
 class TableAssignmentForm(FlaskForm):
     table = SelectField("Table", coerce=int)
     employee = SelectField("Employee", coerce=int)
     assign = SubmitField("Assign")
+
+
+class OrderForm(FlaskForm):
+    table = SelectField("Table", coerce=int)
+    item = SelectField("Item", coerce=int)
+    order = SubmitField("Order")
--- app/routes/orders.py
+++ app/routes/orders.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, current_user
-from app.models import Table, Employee, Order
-from app.forms import TableAssignmentForm
+from app.models import Table, Employee, Order, MenuItem, OrderDetail
+from app.forms import TableAssignmentForm, OrderForm
 from app import db
 from collections import defaultdict
 
@@ -12,10 +12,12 @@ bp = Blueprint("orders", __name__, url_prefix="")
 @login_required
 def index():
     form = TableAssignmentForm()
+    order_form = OrderForm()
 
     tables = db.session.query(Table).all()
     employees = db.session.query(Employee).all()
     open_orders = Order.query.filter(Order.finished == False)
+    menu_items = MenuItem.query.all()
         
     busy_tables = [order.table_id for order in open_orders]
     open_tables = [table for table in tables if table.id not in busy_tables]
@@ -25,6 +27,10 @@ def index():
 
     your_id = int(current_user.get_id())
     your_tables = [table for table in tables if table.orders and table.orders[0].employee.id==your_id]
+
+    
+    order_form.table.choices=[(t.id,t.id) for t in your_tables]
+    order_form.item.choices=[(i.id,i.name) for i in menu_items]
 
     sums=defaultdict(int)
     for table in your_tables:
@@ -38,8 +44,23 @@ def index():
         db.session.add(order)
         db.session.commit()
         return redirect(url_for('.index'))
+    
+    if order_form.validate_on_submit():
+        item=order_form.item.data
+        table=order_form.table.data
+        order_id=Table.query.get(table).orders[0].id
 
-    return render_template('orders.html', tables=tables, form=form, your_tables=your_tables, sums=sums)
+        order_detail=OrderDetail(menu_item_id=item, order_id=order_id)
+        db.session.add(order_detail)
+        db.session.commit()
+        return redirect(url_for('.index'))
+
+    return render_template('orders.html',
+                           tables=tables,
+                           form=form, 
+                           your_tables=your_tables, 
+                           sums=sums, 
+                           order_form=order_form)
 
 @bp.route("/close_table/<int:id>",methods=['POST'])
 @login_required
@@ -52,3 +73,5 @@ def close(id):
     db.session.commit()
 
     return redirect(url_for('.index'))
+
+
